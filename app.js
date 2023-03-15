c6cfb6cc21cfe0bcfd6854221b8e79764a33d6ca
LourdenB15 <lourdenbaydal15@gmail.com>
2023-03-15 15:12:49 +0800

    Add UI

--- app.js
+++ app.js
@@ -1,60 +1,90 @@
-console.log('|||||||||||||||||||||||||||')
-console.log('|||||||||||||||||||||||||||')
-console.log('|||||||||||||||||||||||||||')
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissor = document.querySelector('#scissor');
+const reset = document.querySelector('#reset');
+const playerScore = document.querySelector('#player');
+const computerScore = document.querySelector('#computer');
+const container = document.querySelector('#container');
+const result = document.createElement('div');
+let pScore = 0;
+let cScore = 0;
+let playerSelection;
+let computerSelection;
+rock.addEventListener('click', () => {
+    playerSelection = 'rock'
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+})
+paper.addEventListener('click', () => {
+    playerSelection = 'paper'
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+})
+scissor.addEventListener('click', () => {
+    playerSelection = 'scissor'
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+})
+reset.addEventListener('click', () => {
+    pScore = 0;
+    cScore = 0;
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+    rock.disabled = false;
+    paper.disabled = false;
+    scissor.disabled = false;
+    result.textContent = ""
+    container.appendChild(result);
+})
 
-let usernum;
-let playerScore = 0;
-let computerScore = 0;
-while (playerScore !== 5 && computerScore !== 5) {
-    usernum = prompt('Choose your weapon: (Rock-Paper-Scissor)')
-    while (usernum.toLowerCase() !== 'rock' && usernum.toLowerCase() !== 'paper' && usernum.toLowerCase() !== 'scissor') {
-        usernum = prompt('Invalid choice! Please enter: (Rock-Paper-Scissor)')
-    }
-    let firstChar = usernum.substring(0, 1);
-    let theR = usernum.substring(1, usernum.length).toLowerCase();
-    let playerSelection = firstChar.toUpperCase() + theR
-    let computerSelection = getComputerChoice();
-    console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}`);
-    gameRound(playerSelection, computerSelection)
-    console.log(`player: ${playerScore} || computer: ${computerScore}`)
-    if (playerScore === 5) {
-        console.log('You won! Congratulations!')
-    } else if (computerScore === 5) {
-        console.log('You lost! You better go home!')
-    }
+function playRound(a, b) {
 
-}
-function gameRound(a, b) {
-    if (a.toLowerCase() === b.toLowerCase()) {
-        console.log("It's a draw");
-    } else if (a.toLowerCase() === 'rock' && b.toLowerCase() === 'paper') {
-        console.log("You Lose! Paper beats Rock");
-        computerScore += 1
-    } else if (a.toLowerCase() === 'rock' && b.toLowerCase() === 'scissor') {
-        console.log("You Win! Rock beats Scissor");
-        playerScore += 1
-    } else if (a.toLowerCase() === 'paper' && b.toLowerCase() === 'rock') {
-        console.log("You Win! Paper beats Rock");
-        playerScore += 1
-    } else if (a.toLowerCase() === 'paper' && b.toLowerCase() === 'scissor') {
-        console.log("You Lose! Scissor beats Paper");
-        computerScore += 1
-    } else if (a.toLowerCase() === 'scissor' && b.toLowerCase() === 'rock') {
-        console.log("You Lose! Rock beats Scissor");
-        computerScore += 1
-    } else if (a.toLowerCase() === 'scissor' && b.toLowerCase() === 'paper') {
-        console.log("You Win! Scissor beats Paper");
-        playerScore += 1
+    computerSelection = getComputerChoice();
+    if (a === b) {
+        result.textContent = `It's a draw. The computer chose ${b} as well.`;
+
+    } else if (a === 'rock' && b === 'paper') {
+        result.textContent = "You Lose! Paper beats Rock";
+        cScore += 1;
+    } else if (a === 'rock' && b === 'scissor') {
+        result.textContent = "You Win! Rock beats Scissor";
+        pScore += 1;
+    } else if (a === 'paper' && b === 'rock') {
+        result.textContent = "You Win! Paper beats Rock";
+        pScore += 1;
+    } else if (a === 'paper' && b === 'scissor') {
+        result.textContent = "You Lose! Scissor beats Paper";
+        cScore += 1;
+    } else if (a === 'scissor' && b === 'rock') {
+        result.textContent = "You Lose! Rock beats Scissor";
+        cScore += 1;
+    } else if (a === 'scissor' && b === 'paper') {
+        result.textContent = "You Win! Scissor beats Paper";
+        pScore += 1;
     }
+    if (pScore === 5 || cScore === 5) {
+        if (pScore === 5) {
+            result.textContent = "You won the game! Congratulations!"
+        } else if (cScore === 5) {
+            result.textContent = "You lost! You better go home!"
+        }
+        rock.disabled = true;
+        paper.disabled = true;
+        scissor.disabled = true;
+    }
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+    container.appendChild(result);
 }
 function getComputerChoice() {
     let i = Math.floor(Math.random() * 3) + 1;
     if (i === 1) {
-        i = 'Rock'
+        i = 'rock'
     } else if (i === 2) {
-        i = 'Paper'
+        i = 'paper'
     } else if (i === 3) {
-        i = 'Scissor'
+        i = 'scissor'
     }
     return i
 }
+
